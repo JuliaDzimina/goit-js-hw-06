ce00b73bf2d6a62209d5c1826185b68e39c8080c
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2023-11-10 12:16:46 +0200

    task3

--- js/task-3.js
+++ js/task-3.js
@@ -6,3 +6,4 @@ return totalWith;
 console.log(getElementWidth("50px", "8px", "4px")); 
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px")); 
+
