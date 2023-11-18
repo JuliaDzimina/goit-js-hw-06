81a6d4d37bfbdf9856c22439e8a69b5963dfc955
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2023-11-18 23:18:59 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,17 @@
-function makeTransaction(quantity, pricePerDroid){
-    const totalPrice = quantity * pricePerDroid;
-    const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`
-   return message
+// Задача 1. Генератор slug
+
+// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+// Усі символи slug повинні бути в нижньому регістрі.
+// Усі слова slug повинні бути розділені тире.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+function slugify(title){
+    const arrayTitle = title.split(" ");
+    const stringTitle = arrayTitle.join("-").toLowerCase();
+    return stringTitle;
 }
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000)); 
-console.log(makeTransaction(10, 500));
+console.log("Task-1:", slugify("Arrays for begginers")); 
+console.log("Task-1:", slugify("English for developer")); 
+console.log("Task-1:", slugify("Ten secrets of JavaScript")); 
+console.log("Task-1:", slugify("How to become a JUNIOR developer in TWO WEEKS")); 
